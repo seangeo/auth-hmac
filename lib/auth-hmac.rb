9c19d9f728aa7451931acbf62e0024ecc81fa267
Sean Geoghegan <seangeo@gmail.com>
2008-07-10 12:26:11 +0930

    Added building of canonical strings

--- lib/auth-hmac.rb
+++ lib/auth-hmac.rb
@@ -1,6 +1,48 @@
+# Copyright (c) 2008 The Kaphan Foundation
+#
+# Possession of a copy of this file grants no permission or license
+# to use, modify, or create derivate works.
+# Please contact info@peerworks.org for further information.
+#
+
 $:.unshift(File.dirname(__FILE__)) unless
   $:.include?(File.dirname(__FILE__)) || $:.include?(File.expand_path(File.dirname(__FILE__)))
 
+# This module provides a HMAC Authentication method for HTTP requests.
+#
+# It is loosely based on the Amazon Web Services Authentication mechanism but
+# generalized to be useful to any application that requires HMAC based authentication.
+# As a result of the generalization, it won't work with AWS because it doesn't support
+# the Amazon extension headers.
+#
 module AuthHMAC
-  
+  # Build a Canonical String for a HTTP request.
+  #
+  # A Canonical String has the following format:
+  #
+  # CanonicalString = HTTP-Verb    + "\n" +
+  #                   Content-Type + "\n" +
+  #                   Content-MD5  + "\n" +
+  #                   Date         + "\n" +
+  #                   request-uri;
+  #
+  class CanonicalString < String
+    def initialize(request)
+      self << request.method + "\n"
+      self << header_values(request) + "\n"
+      self << request_path(request)
+    end
+    
+    private
+      def header_values(request)
+        [ request.content_type, 
+          request['content-md5'], 
+          request['date']
+        ].join("\n")
+      end
+      
+      def request_path(request)
+        request.path[/^[^?]*/]
+      end
+  end
 end
--- spec/auth-hmac_spec.rb
+++ spec/auth-hmac_spec.rb
@@ -1,11 +1,71 @@
 require File.dirname(__FILE__) + '/spec_helper.rb'
+require "net/http"
+require 'time'
 
-# Time to add your specs!
-# http://rspec.info/
-describe "Place your specs here" do
+include AuthHMAC
+describe AuthHMAC do
   
-  it "find this spec in spec directory" do
-    violated "Be sure to write your specs"
+  describe CanonicalString do
+    it "should include the http verb when it is GET" do
+      request = Net::HTTP::Get.new("/")
+      CanonicalString.new(request).should match(/GET/)
+    end
+    
+    it "should include the http verb when it is POST" do
+      request = Net::HTTP::Post.new("/")
+      CanonicalString.new(request).should match(/POST/)
+    end
+    
+    it "should include the content-type" do
+      request = Net::HTTP::Put.new("/", {'Content-Type' => 'application/xml'})
+      CanonicalString.new(request).should match(/application\/xml/)
+    end
+    
+    it "should include the content-type even if the case is messed up" do
+      request = Net::HTTP::Put.new("/", {'cOntent-type' => 'text/html'})
+      CanonicalString.new(request).should match(/text\/html/)
+    end
+    
+    it "should include the content-md5" do
+      request = Net::HTTP::Put.new("/", {'Content-MD5' => 'skwkend'})
+      CanonicalString.new(request).should match(/skwkend/)
+    end    
+    
+    it "should include the content-md5 even if the case is messed up" do
+      request = Net::HTTP::Put.new("/", {'content-md5' => 'adsada'})
+      CanonicalString.new(request).should match(/adsada/)
+    end
+    
+    it "should include the date" do
+      date = Time.now.httpdate
+      request = Net::HTTP::Put.new("/", {'Date' => date})
+      CanonicalString.new(request).should match(/#{date}/)
+    end
+    
+    it "should include the request path" do
+      request = Net::HTTP::Get.new("/path/to/file")
+      CanonicalString.new(request).should match(/\/path\/to\/file[^?]?/)
+    end
+    
+    it "should ignore the query string of the request path" do
+      request = Net::HTTP::Get.new("/other/path/to/file?query=foo")
+      CanonicalString.new(request).should match(/\/other\/path\/to\/file[^?]?/)
+    end
+    
+    it "should build the correct string" do
+      date = Time.now.httpdate
+      request = Net::HTTP::Put.new("/path/to/put?foo=bar&bar=foo", 
+                                    'content-type' => 'text/plain', 
+                                    'content-md5' => 'blahblah', 
+                                    'date' => date)
+      CanonicalString.new(request).should == "PUT\ntext/plain\nblahblah\n#{date}\n/path/to/put"                                            
+    end
+    
+    it "should build the correct string when some elements are missing" do
+      date = Time.now.httpdate
+      request = Net::HTTP::Get.new("/path/to/get?foo=bar&bar=foo",
+                                    'date' => date)
+      CanonicalString.new(request).should == "GET\n\n\n#{date}\n/path/to/get"
+    end
   end
-  
 end
