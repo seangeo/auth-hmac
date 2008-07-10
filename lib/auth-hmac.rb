73abe309abe6361e27a79e1bb05c8158dd910829
Sean Geoghegan <seangeo@gmail.com>
2008-07-10 13:02:42 +0930

    Added sign method that signs a request.

--- lib/auth-hmac.rb
+++ lib/auth-hmac.rb
@@ -8,14 +8,48 @@
 $:.unshift(File.dirname(__FILE__)) unless
   $:.include?(File.dirname(__FILE__)) || $:.include?(File.expand_path(File.dirname(__FILE__)))
 
-# This module provides a HMAC Authentication method for HTTP requests.
+require 'openssl'
+require 'base64'
+
+# This module provides a HMAC Authentication method for HTTP requests. It should work with
+# net/http request classes and CGIRequest classes and hence Rails.
 #
 # It is loosely based on the Amazon Web Services Authentication mechanism but
 # generalized to be useful to any application that requires HMAC based authentication.
 # As a result of the generalization, it won't work with AWS because it doesn't support
 # the Amazon extension headers.
 #
-module AuthHMAC
+class AuthHMAC
+  
+  def initialize(credential_store)
+    @credential_store = credential_store
+  end
+  
+  # Signs a request using the access_key_id and the secret associated with that id
+  # in the credential store.
+  #
+  # Signing a requests adds an Authorization header to the request in the format:
+  #
+  #  AuthHMAC <access_key_id>:<signature>
+  #
+  # where <signature> is the Base64 encoded HMAC-SHA1 of the CanonicalString and the secret.
+  #
+  def sign!(request, access_key_id)
+    secret = @credential_store[access_key_id]
+    raise ArgumentError, "No secret found for key id '#{access_key_id}'" if secret.nil?
+    request['Authorization'] = build_authorization_header(request, access_key_id, secret)
+  end
+  
+  private
+    def build_authorization_header(request, access_key_id, secret)
+      "AuthHMAC #{access_key_id}:#{build_signature(request, secret)}"      
+    end
+    
+    def build_signature(request, secret)
+      digest = OpenSSL::Digest::Digest.new('sha1')
+      Base64.encode64(OpenSSL::HMAC.digest(digest, secret, CanonicalString.new(request))).strip
+    end
+  
   # Build a Canonical String for a HTTP request.
   #
   # A Canonical String has the following format:
--- spec/auth-hmac_spec.rb
+++ spec/auth-hmac_spec.rb
@@ -2,54 +2,101 @@ require File.dirname(__FILE__) + '/spec_helper.rb'
 require "net/http"
 require 'time'
 
-include AuthHMAC
 describe AuthHMAC do
+  describe ".sign!" do
+    before(:each) do
+      @store = mock('store')
+      @store.stub!(:[]).and_return("")
+      @authhmac = AuthHMAC.new(@store)
+    end
+    
+    it "should add an Authorization header" do
+      request = Net::HTTP::Get.new("/")
+      @authhmac.sign!(request, 'key-id')
+      request.key?("Authorization").should be_true
+    end
+    
+    it "should fetch the secret from the store" do
+      request = Net::HTTP::Get.new("/")
+      @store.should_receive(:[]).with('key-id').and_return('secret')
+      @authhmac.sign!(request, 'key-id')
+    end
+    
+    it "should prefix the Authorization Header with AuthHMAC" do
+      request = Net::HTTP::Get.new("/")
+      @authhmac.sign!(request, 'key-id')
+      request['Authorization'].should match(/^AuthHMAC /)
+    end
+    
+    it "should include the key id as the first part of the Authorization header value" do
+      request = Net::HTTP::Get.new("/")
+      @authhmac.sign!(request, 'key-id')
+      request['Authorization'].should match(/^AuthHMAC key-id:/)
+    end
+    
+    it "should include the base64 encoded HMAC signature as the last part of the header value" do
+      request = Net::HTTP::Get.new("/path")
+      @authhmac.sign!(request, 'key-id')
+      request['Authorization'].should match(/:LANuM6GQA23WwIFdfC3IXm60dx4=$/)
+    end
+    
+    it "should create a complete signature" do
+      @store.should_receive(:[]).with('my-key-id').and_return('secret')
+      date = "Thu, 10 Jul 2008 03:29:56 GMT"
+      request = Net::HTTP::Put.new("/path/to/put?foo=bar&bar=foo", 
+                                    'content-type' => 'text/plain', 
+                                    'content-md5' => 'blahblah', 
+                                    'date' => date)
+      @authhmac.sign!(request, "my-key-id")
+      request['Authorization'].should == "AuthHMAC my-key-id:71wAJM4IIu/3o6lcqx/tw7XnAJs="
+    end
+  end
   
-  describe CanonicalString do
+  describe AuthHMAC::CanonicalString do
     it "should include the http verb when it is GET" do
       request = Net::HTTP::Get.new("/")
-      CanonicalString.new(request).should match(/GET/)
+      AuthHMAC::CanonicalString.new(request).should match(/GET/)
     end
     
     it "should include the http verb when it is POST" do
       request = Net::HTTP::Post.new("/")
-      CanonicalString.new(request).should match(/POST/)
+      AuthHMAC::CanonicalString.new(request).should match(/POST/)
     end
     
     it "should include the content-type" do
       request = Net::HTTP::Put.new("/", {'Content-Type' => 'application/xml'})
-      CanonicalString.new(request).should match(/application\/xml/)
+      AuthHMAC::CanonicalString.new(request).should match(/application\/xml/)
     end
     
     it "should include the content-type even if the case is messed up" do
       request = Net::HTTP::Put.new("/", {'cOntent-type' => 'text/html'})
-      CanonicalString.new(request).should match(/text\/html/)
+      AuthHMAC::CanonicalString.new(request).should match(/text\/html/)
     end
     
     it "should include the content-md5" do
       request = Net::HTTP::Put.new("/", {'Content-MD5' => 'skwkend'})
-      CanonicalString.new(request).should match(/skwkend/)
+      AuthHMAC::CanonicalString.new(request).should match(/skwkend/)
     end    
     
     it "should include the content-md5 even if the case is messed up" do
       request = Net::HTTP::Put.new("/", {'content-md5' => 'adsada'})
-      CanonicalString.new(request).should match(/adsada/)
+      AuthHMAC::CanonicalString.new(request).should match(/adsada/)
     end
     
     it "should include the date" do
       date = Time.now.httpdate
       request = Net::HTTP::Put.new("/", {'Date' => date})
-      CanonicalString.new(request).should match(/#{date}/)
+      AuthHMAC::CanonicalString.new(request).should match(/#{date}/)
     end
     
     it "should include the request path" do
       request = Net::HTTP::Get.new("/path/to/file")
-      CanonicalString.new(request).should match(/\/path\/to\/file[^?]?/)
+      AuthHMAC::CanonicalString.new(request).should match(/\/path\/to\/file[^?]?/)
     end
     
     it "should ignore the query string of the request path" do
       request = Net::HTTP::Get.new("/other/path/to/file?query=foo")
-      CanonicalString.new(request).should match(/\/other\/path\/to\/file[^?]?/)
+      AuthHMAC::CanonicalString.new(request).should match(/\/other\/path\/to\/file[^?]?/)
     end
     
     it "should build the correct string" do
@@ -58,14 +105,14 @@ describe AuthHMAC do
                                     'content-type' => 'text/plain', 
                                     'content-md5' => 'blahblah', 
                                     'date' => date)
-      CanonicalString.new(request).should == "PUT\ntext/plain\nblahblah\n#{date}\n/path/to/put"                                            
+      AuthHMAC::CanonicalString.new(request).should == "PUT\ntext/plain\nblahblah\n#{date}\n/path/to/put"                                            
     end
     
     it "should build the correct string when some elements are missing" do
       date = Time.now.httpdate
       request = Net::HTTP::Get.new("/path/to/get?foo=bar&bar=foo",
                                     'date' => date)
-      CanonicalString.new(request).should == "GET\n\n\n#{date}\n/path/to/get"
+      AuthHMAC::CanonicalString.new(request).should == "GET\n\n\n#{date}\n/path/to/get"
     end
   end
 end
